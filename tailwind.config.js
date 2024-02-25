d37b36649ae47a03235875bf6eb94a3a177cbb9b
rafaeldevvv <115797599+rafaeldevvv@users.noreply.github.com>
2024-02-25 10:37:56 -0300

    feat: load font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html"],
   theme: {
+    colors: {
+      green: "hsl(75, 94%, 57%)",
+      white: "hsl(0, 0%, 100%)",
+      gray: "hsl(0, 0%, 20%)",
+      darkGray: "hsl(0, 0%, 12%)",
+      offBlack: "hsl(0, 0%, 8%)"
+    },
+    fontFamily: {
+      sans: ["Inter", ...defaultTheme.fontFamily.sans]
+    },
     extend: {},
   },
   plugins: [],
